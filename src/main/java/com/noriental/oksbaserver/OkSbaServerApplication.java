8c37e052738e9429ca9bc140e539f031fea4a443
unclebryan719 <unclebryan719@gmail.com>
2022-09-08 16:16:10 +0800

    init

---
+++ src/main/java/com/noriental/oksbaserver/OkSbaServerApplication.java
@@ -0,0 +1,14 @@
+package com.noriental.oksbaserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableAdminServer
+@SpringBootApplication
+public class OkSbaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkSbaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noriental/oksbaserver/OkSbaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noriental.oksbaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkSbaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
